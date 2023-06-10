513e846f3a36f88f4bf792a800a1ee2ba005cc01
BuildTools <unconfigured@null.spigotmc.org>
2023-06-10 22:09:42 +0200

    Not Enough Wooden Items 1.0.0

---
+++ src/main/java/fr/m336/notenoughwoodenitems/NotEnoughWoodenItemsClient.java
@@ -0,0 +1,10 @@
+package fr.m336.notenoughwoodenitems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NotEnoughWoodenItemsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
